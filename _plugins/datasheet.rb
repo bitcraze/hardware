71c489259daa3093b78790c8b010bd5a40bf417a
Marcus Eliasson <marcus@bitcraze.se>
2020-04-07 11:07:40 +0200

    Added notice box to layout (#4)

--- _plugins/datasheet.rb
+++ _plugins/datasheet.rb
@@ -63,8 +63,47 @@ module Jekyll
         '<img class="ds-img-%1$s-%2$s" src="%3$s" />' % [@alignment, @size, @image]
       end
     end
+
+    class DatasheetNotice < Liquid::Block
+      include Jekyll::PluginHelper
+
+      # Used to insert a note into the datasheet
+      #
+      # Takes one argument, the type and the markdown
+      # is inside the block
+      #
+      # Example
+      #
+      # {% datasheet_notice warning; %}
+      # Some notice here!
+      # {% enddatasheet_notice %}
+      #
+      # Supported types: warning (yellow), danger (red), secondary (grey)
+
+      def initialize(tag_name, text, tokens)
+        super
+        params = parse_args(text)
+
+        @type = params[0]
+      end
+
+      def render(context)
+        '<div class="row">
+          <div class="col-sm-1">
+          </div>
+          <div class="col-sm-10">
+            <div class="alert alert-%1$s" role="alert">
+              %2$s
+            </div>
+          </div>
+          <div class="col-sm-1">
+          </div>
+        </div>' % [@type, markdownify(super, context)]
+      end
+    end
   end
 end
 
 Liquid::Template.register_tag('datasheet_intro', Jekyll::Datasheet::DatasheetIntro)
 Liquid::Template.register_tag('datasheet_img', Jekyll::Datasheet::DatasheetImg)
+Liquid::Template.register_tag('datasheet_notice', Jekyll::Datasheet::DatasheetNotice)
