d23798f7dce97f1772661c9afc881808894a7682
Marcus Eliasson <marcus@bitcraze.se>
2020-04-15 13:23:31 +0200

    Use dockerized wkhtmltopdf instead (closes #3)

--- tools/generate.sh
+++ tools/generate.sh
@@ -12,11 +12,8 @@ set -e
 scriptDir=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 rootDir=$scriptDir/..
 
-# Write base path to local Jekyll config
-echo "url: ${rootDir}" > ${rootDir}/_local_config.yml
-
 # Execute Jekyll
-cmd="docker run --rm -it --volume=${rootDir}:/module bitcraze/web-builder jekyll build --config _config.yml,_local_config.yml --trace"
+cmd="docker run --rm -it --volume=${rootDir}:/module bitcraze/web-builder jekyll build --config _config.yml --trace"
 echo "$cmd"
 if $cmd; then
   # Create docs directory if it doesn't exist
@@ -34,7 +31,7 @@ if $cmd; then
     then
       echo $product
       echo $1
-      cmd="wkhtmltopdf --margin-top 10 --margin-bottom 10 --header-spacing 5 --footer-html ${rootDir}/assets/html/datasheet-footer.html --header-html ${rootDir}/assets/html/datasheet-header.html --viewport-size 1280x1024 ${rootDir}/${fname} ${rootDir}/docs/${product}-datasheet.pdf"
+      cmd="docker run -v ${rootDir}:/root --rm icalialabs/wkhtmltopdf:alpine --margin-top 10 --margin-bottom 10 --header-spacing 5 --footer-html /root/assets/html/datasheet-footer.html --header-html /root/assets/html/datasheet-header.html --viewport-size 1280x1024 /root/${fname} /root/docs/${product}-datasheet.pdf"
       echo ${cmd}
       `${cmd}`
     fi
